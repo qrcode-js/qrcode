55ee11a999efb7257acbc7d09b9c21aca19667b4
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2022-09-10 14:40:57 +0200

    CLI: another rework

--- packages/node/src/cli.ts
+++ packages/node/src/cli.ts
@@ -7,15 +7,23 @@ import type { Options } from "@qrcode-js/core";
 async function main(): Promise<void> {
   yargs(hideBin(process.argv))
     .scriptName("qrcode")
+    .usage(
+      "$0 <output_path> [options...]\n\nAll options are prefixed with '--opt.'. Example: --opt.text 'this is a text', or --opt.margin.size 40"
+    )
     .command(
-      "$0 [options] <output_path>",
-      "All options are prefixed with '--opt.'. Example: --opt.text 'this is a text', or --opt.margin.size 40",
-      {},
+      "$0 <output_path>",
+      "",
+      (yargs) => {
+        yargs.option("opt", {
+          string: true,
+          describe: "Used to set options for creating the QR. See above.",
+          default: {},
+        });
+      },
       handleDefaultCommand
     )
-    .option("opt", {
-      default: {},
-    })
+    .wrap(null)
+    .strict()
     .parseSync();
 }
 
